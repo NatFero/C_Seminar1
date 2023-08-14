800b91a9452b8e5166bedc1735846475be11063f
Prusakova_Natalia <suminanatali@gmail.com>
2023-08-14 14:29:34 +0300

    seminar1 lesson1

---
+++ Lesson_1_Home/Program.cs
@@ -0,0 +1,92 @@
+﻿// Задача 1
+// принимает число и выдат его квадрат (число уможенное само на себя)
+
+//Console.WriteLine("Введите число");
+//int num =Convert.ToInt32(Console.ReadLine());
+//int result = num*num;
+//Console.WriteLine(result);
+
+
+// Задача 2
+// проверяем является ли первое число квадратом второго
+
+
+//Console.WriteLine("Введите число");
+//int num1 =Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Введите число");
+//int num2 =Convert.ToInt32(Console.ReadLine());
+
+//if(num1 == num2*num2)
+//{
+ //   Console.WriteLine("Да");
+//}
+//else
+//{
+//    Console.WriteLine("Нет");
+//}
+
+
+
+// Задача 3
+// Дни недели
+// Console.WriteLine("Введите число");
+// int num1 =Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (num1 == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (num1 == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (num1 == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (num1 == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (num1 == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (num1 == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня недели нет");
+// }
+
+
+
+// Задача 4
+// На вход оддно число (N), а на выход все от -N до N
+
+// Console.WriteLine("Введите число");
+// int num1 =Convert.ToInt32(Console.ReadLine());
+
+// int numNeg= num1 *-1;
+
+// while (numNeg<=num1)
+// {
+//     Console.WriteLine(numNeg);
+//     numNeg=numNeg+1;
+// }
+
+
+
+// Задача 5
+// Остаток от деления
+Console.WriteLine("Введите число");
+int num1 =Convert.ToInt32(Console.ReadLine());
+int result = num1 % 10;
+Console.WriteLine(result);
